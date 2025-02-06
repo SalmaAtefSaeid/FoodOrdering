fd16e91cf9af068d7a0206cf5458c24ed9c776e7
SalmaAtefSaeid <salma.atef.ebrahim@gmail.com>
2025-02-06 17:14:52 +0200

    Initial Commit

---
+++ FoodOrdering/FoodOrderingApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodOrderingApp.swift
+//  FoodOrdering
+//
+//  Created by Jasmine on 06/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct FoodOrderingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
